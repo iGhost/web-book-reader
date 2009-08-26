9a55abafba9b74242ea08b82cc7afe25f4e808a7
Gizzat Ablakov <gizzat@gmail.com>
2009-08-26 17:07:47 +0600

    Fixing comments, language, code

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?
 $q='';
-//$d='/home/localhost/www/book/';
-//$d='/var/www/book/';
 $d=$_SERVER['DOCUMENT_ROOT'].'/book/';
-/*echo '<pre>';
-print_r($_FILES);
-echo '</pre>';*/
 if(isset($_POST['name']))
 {
 	$dir=$d.$_POST['name'];
@@ -17,33 +12,32 @@ if(isset($_POST['name']))
 			mkdir($dir);
 			if(move_uploaded_file($_FILES['bookfile']['tmp_name'],$src))
 			{
-				$q.='<span class="ok">Файл загружен.</span><br>';
-				$q.='<span class="ok">Размещаем файл в директорию ['.$_POST['name'].']</span><br>';
-				//$q.='<span class="ok">Начинаем расчленение.</span><br>';
+				$q.='<span class="ok">File uploaded.</span><br>';
+				$q.='<span class="ok">Moving file to ['.$_POST['name'].'] directory.</span><br>';
+				//$q.='<span class="ok">Starting split operation.</span><br>';
 				$a=file_get_contents($src);
 /*				while(substr(...))
 				{
 					
 				}*/
-				$q.='<span class="ok"><a href="/book/'.$_POST['name'].'/0.htm">Ссылка на <b>полную</b> книгу</a></span><br>';
+				$q.='<span class="ok"><a href="/book/'.$_POST['name'].'/0.htm">Link to a <b>full</b> book version</a></span><br>';
 			}
 			else
 			{
-				$q.='<span class="error">Файл не загружен!</span>';
+				$q.='<span class="error">File uploaded correctly!</span>';
 				rmdir($dir);
 			}
 		}
 		else
-			$q.='<span class="error">Файл не указан!</span>';
+			$q.='<span class="error">File have not specified!</span>';
 	}
 	else
-		$q.='<span class="error">Папка с таким именем уже имеется!</span>';
+		$q.='<span class="error">Folder already exists!</span>';
 }
-
-
 ?>
 <html>
 <head>
+<title>Web Book Reader</title>
 <style>
 body,table{
 	background-color:#dddddd;
@@ -70,19 +64,19 @@ input{
 
 <br><br>
 
-Залей книгу тут!<br/>
-<table border=0 style="border:2px solid #aaa"><tr><td>
+Upload book here!<br/>
+<table border="0" style="border:2px solid #aaa"><tr><td>
 <form action="" method="post" enctype="multipart/form-data">
 
-файл:<br/>
+book file:<br/>
 <input name="bookfile" type="file"/><br/><br/>
 
-размер страницы:<br/>
+page size:<br/>
 <input name="pagesize" type="text" value="10240" size="6"/><br/><br/>
 
-название:<br>
+book name:<br>
 <input name="name" type="text" value="<?=rand(1000,9999)?>"/><br/>
-<span class="comment">Навание книги<br/> по которому к нему будет<br/> идти обращение.<br/> (латинскими буквами)</span>
+<span class="comment">Book name<br/> to access it by.</span>
 <br/><br/>
 <input type="submit" value="Расчленить"/>
 </form>
